aeb76376d0b630d7d1688cb9d78c985411f35031
Bennett Hardwick <bennetthardwick@icloud.com>
2017-10-05 00:20:03 +1000

    Created reload script

---
+++ .config/reload.sh
@@ -0,0 +1,8 @@
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar example
