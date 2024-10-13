67e2e43a90072b6dfc87ca12770fc6320f1bef56
Bennett Hardwick <hello@bennett.dev>
2024-10-14 09:19:18 +1100

    Update

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,5 +4,6 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox dark, medium (base16)'
 config.audible_bell = 'Disabled'
+config.harfbuzz_features = { 'calt=0' }
 
 return config
